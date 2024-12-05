f1296d9daa32f62860f4be568a7fb8d682b344d1
BugHunter-X <askzenai@gmail.com>
2024-12-05 03:42:06 +0100

    Add FlatListBug.js file

---
+++ FlatListBug.js
@@ -0,0 +1,1 @@
+This error occurs when using the FlatList component in React Native and trying to render a large number of items. It happens because the FlatList is trying to measure the height of each item before rendering, which can lead to performance issues and ultimately crash the app.
